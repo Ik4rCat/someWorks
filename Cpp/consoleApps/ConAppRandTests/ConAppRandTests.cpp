2a599327dd34ba7b9e1d6e0c41ae8ed935506edf
ALT <119232131+Ik4rCat@users.noreply.github.com>
2025-11-05 15:01:00 +0300

    feat&refactor(C.A.R.T cpp)<alt>:none

--- Cpp/consoleApps/ConAppRandTests/ConAppRandTests.cpp
+++ Cpp/consoleApps/ConAppRandTests/ConAppRandTests.cpp
@@ -1,12 +1,16 @@
 ﻿#include <iostream>
 #include <string>
 #include <ctime>
+#include <locale>
+
 using namespace std;
 
 int main()
 {
 	/// HI, Here i testing random in cpp on my cpp college lessons ＼（〇_ｏ）／
 	/// I'm interested in how it works in Unreal engine (。_。)
+	setlocale(LC_ALL, "ru");
+
 	int x, y, i;
 
 	cout << "Start num" << endl;
@@ -19,28 +23,40 @@ int main()
 	
 	cout << "your random num = " << i << "\n" << endl;
 
-	switch (i) {
-	case(0):
-		cout << "bruh... how" << endl;
+	switch (rand() % (10 - 1+1)+1) {
+	case 0:
+		cout << "Возрат каретки в начало строки " << "\r" << endl;
 		break;
-	case(1):
-		cout << "noob" << endl;
+	case 1:
+		cout << "Новая строка " << "\n" << endl;
 		break;
-	case(2):
-		cout << "GOD" << endl;
+	case 2:
+		cout << "Горизонтальная табуляция " << "\t" << endl;
 		break;
-	case(3):
-		cout << "lol" << endl;
+	case 3:
+		cout << "Вертикальная табуляция " << "\v" << endl;
 		break;
-	case(4):
-		cout << "nice" << endl;
+	case 4:
+		cout << "двойные кавычки " << "\1" << endl;
 		break;
-	case(5):
-		cout << "good" << endl;
+	case 5:
+		cout << "Апостров " << "\'" << endl;
+		break;
+	case 6:
+		cout << "обратный слеш " << "\\" << endl;
+		break;
+	case 7:
+		cout << "Нулевой символ " << "\0" << endl;
+		break;
+	case 8:
+		cout << "Знак вопроса " << "\?" << endl;
+		break;
+	case 9:
+		cout << "Сигнал бипера (спикера) компьютера " << "\a" << endl;
 		break;
 	}
 
-	cout << "Some random nums \n" << endl;
+	cout << "\nSome random nums \n" << endl;
 	cout << "random num first = " << rand() % 100 + 1 << endl;
 	cout << "random num second = " << rand()  << endl;
 	cout << "random num third = " << rand()  << endl;
