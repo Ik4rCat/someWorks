e4037a303c5399d6b3ebf11410bc1ab4f1905b68
ALT <119232131+Ik4rCat@users.noreply.github.com>
2025-11-05 13:52:45 +0300

    feat(C.A.R.T cpp)<alt>:none

--- Cpp/consoleApps/ConAppRandTests/ConAppRandTests.cpp
+++ Cpp/consoleApps/ConAppRandTests/ConAppRandTests.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+	/// HI, Here i test random ＼（〇_ｏ）／
+	
+	srand(9);
+	cout << "random num first = " << rand() % (30 - 11 + 1) + 11 << endl;
+	cout << "random num second = " << rand()  << endl;
+	cout << "random num third = " << rand()  << endl;
+
+	return 0;
 }
