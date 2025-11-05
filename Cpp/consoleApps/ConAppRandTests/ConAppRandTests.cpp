f700f5c8a6a8900193b41377d383758ceccffc49
ALT <119232131+Ik4rCat@users.noreply.github.com>
2025-11-05 14:29:32 +0300

    feat&refactor(C.A.R.T cpp)<alt>:user can give start & end num for random num

--- Cpp/consoleApps/ConAppRandTests/ConAppRandTests.cpp
+++ Cpp/consoleApps/ConAppRandTests/ConAppRandTests.cpp
@@ -1,13 +1,47 @@
 ﻿#include <iostream>
 #include <string>
+#include <ctime>
 using namespace std;
 
 int main()
 {
-	/// HI, Here i test random ＼（〇_ｏ）／
+	/// HI, Here i testing random in cpp on my cpp college lessons ＼（〇_ｏ）／
+	/// I'm interested in how it works in Unreal engine (。_。)
+	int x, y, i;
+
+	cout << "Start num" << endl;
+	cin >> x;
+	cout << "End num" << endl;
+	cin >> y;
+
+	srand(time(0));
+	i = rand() % (y - x + 1) + x;
 	
-	srand(9);
-	cout << "random num first = " << rand() % (30 - 11 + 1) + 11 << endl;
+	cout << "your random num = " << i << "\n" << endl;
+
+	switch (i) {
+	case(0):
+		cout << "bruh... how" << endl;
+		break;
+	case(1):
+		cout << "noob" << endl;
+		break;
+	case(2):
+		cout << "GOD" << endl;
+		break;
+	case(3):
+		cout << "lol" << endl;
+		break;
+	case(4):
+		cout << "nice" << endl;
+		break;
+	case(5):
+		cout << "good" << endl;
+		break;
+	}
+
+	cout << "Some random nums \n" << endl;
+	cout << "random num first = " << rand() % 100 + 1 << endl;
 	cout << "random num second = " << rand()  << endl;
 	cout << "random num third = " << rand()  << endl;
 
