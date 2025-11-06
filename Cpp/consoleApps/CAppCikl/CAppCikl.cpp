5cad785f06c4500d32244e1e213671234712c161
ALT <119232131+Ik4rCat@users.noreply.github.com>
2025-11-06 12:59:58 +0300

    feat(C.A.C)<alt>

--- Cpp/consoleApps/CAppCikl/CAppCikl.cpp
+++ Cpp/consoleApps/CAppCikl/CAppCikl.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <iomanip>
 #include <string>
 using namespace std;
 
@@ -9,32 +10,42 @@ int main()
 	/// enjoy it!
 
 	int userChoice, userInp;
+	
+	while (true)
+	{
+		cout <<"\nWrite number u like : \n 1) friendly numbers \n 2) evil numbers \n 3)* table" << endl;
+		cin >> userChoice;
 
-	cout << "Write number u like: \n 1) friendly numbers \n 2) evil numbers \n 3) * table" << endl;
-	cin >> userChoice;
+		switch (userChoice) {
+		case 1:
+			cout << "Write number u like: " << endl;
+			cin >> userInp;
+			for (int i = 0; userInp >= i; i++) {
+				int ipp = i + 2;
+				cout << "\n" << i << " " << i + 1 << " " << ipp << endl;
+				i = ipp;
+			}
+			break;
 
-	if (userChoice == 1) {
-				cout << "Write number u like: " << endl;
-		cin >> userInp;
-		for (int i = 0; userInp >= i; i++) {
-			int ipp = i + 2;
-			cout << "\n" << i << " " << i + 1 << " " << ipp << endl;
-			i = ipp;
+		case 2:
+			cout << "Write number u like: " << endl;
+			cin >> userInp;
+
+			for (int i = -10; userInp >= i; i++) {
+				int ipp = i + 2;
+				cout << "\n" << i << " " << i + 1 << " " << ipp << endl;
+				i = ipp;
+			}
+			break;
+
+		case 3:
+			for (int i = 1; i <= 10; ++i) {
+				for (int j = 1; j <= 10; ++j) {
+					int result = i * j;
+					cout << i << " * " << j << " = " << setw(3) << result << "\t";
+				}
+				cout << endl;
+			}
 		}
-		return 0;
 	}
-	else if (userChoice == 2) {
-		cout << "Write number u like: " << endl;
-		cin >> userInp;
-
-		for (int i = -10; userInp >= i; i++) {
-			int ipp = i + 2;
-			cout << "\n" << i << " " << i + 1 << " " << ipp << endl;
-			i = ipp;
-		}
-	}
-	else if (userChoice == 3) {
-
-	}
-
 }
