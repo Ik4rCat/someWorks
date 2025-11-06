f12894d7cb1d921c4db285678085a59c28a92545
ALT <119232131+Ik4rCat@users.noreply.github.com>
2025-11-06 12:25:14 +0300

    feat(C.A.C)<alt>

--- Cpp/consoleApps/CAppCikl/CAppCikl.cpp
+++ Cpp/consoleApps/CAppCikl/CAppCikl.cpp
@@ -2,7 +2,19 @@
 #include <string>
 using namespace std;
 
-int main()
+int main() 
 {
-    
+	/// HI, here is CAppCikl.cpp
+	/// I testing for loops in C++ ^o^
+	/// enjoy it!
+
+	int userInp;
+
+	cout << "Write number u like: " << endl;
+	cin >> userInp;
+
+	for (int i = -10; userInp >= i; i++) {
+		cout << "\n" << i << endl;
+	}
+
 }
