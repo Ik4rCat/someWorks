2acd78321d86f8447fbe1b7a837653eb605f86bb
ALT <119232131+Ik4rCat@users.noreply.github.com>
2025-11-06 12:40:03 +0300

    feat(C.A.C)<alt>

--- Cpp/consoleApps/CAppCikl/CAppCikl.cpp
+++ Cpp/consoleApps/CAppCikl/CAppCikl.cpp
@@ -8,13 +8,33 @@ int main()
 	/// I testing for loops in C++ ^o^
 	/// enjoy it!
 
-	int userInp;
+	int userChoice, userInp;
 
-	cout << "Write number u like: " << endl;
-	cin >> userInp;
+	cout << "Write number u like: \n 1) friendly numbers \n 2) evil numbers \n 3) * table" << endl;
+	cin >> userChoice;
 
-	for (int i = -10; userInp >= i; i++) {
-		cout << "\n" << i << endl;
+	if (userChoice == 1) {
+				cout << "Write number u like: " << endl;
+		cin >> userInp;
+		for (int i = 0; userInp >= i; i++) {
+			int ipp = i + 2;
+			cout << "\n" << i << " " << i + 1 << " " << ipp << endl;
+			i = ipp;
+		}
+		return 0;
+	}
+	else if (userChoice == 2) {
+		cout << "Write number u like: " << endl;
+		cin >> userInp;
+
+		for (int i = -10; userInp >= i; i++) {
+			int ipp = i + 2;
+			cout << "\n" << i << " " << i + 1 << " " << ipp << endl;
+			i = ipp;
+		}
+	}
+	else if (userChoice == 3) {
+
 	}
 
 }
