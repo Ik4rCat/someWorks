e0f4abadbda86c44e9dd8b698055fd623f4e1c4e
ALT <119232131+Ik4rCat@users.noreply.github.com>
2025-11-07 14:27:52 +0300

    feat(CTApp)<alt>

--- Cpp/consoleApps/CTApp/CTApp.cpp
+++ Cpp/consoleApps/CTApp/CTApp.cpp
@@ -6,7 +6,53 @@ using namespace std;
 int main()
 {
 	/// HI, i need to make my taskes (。_。) 
+	Menu();
+}
 
-	
+void Menu() 
+{
 
+	int choice;
+
+	while (true) {
+
+	}
+	cout << "Select task: \n 1 - Task I" << endl;
+	cout << "" << endl;
+
+	cin >> choice;
+
+	// cout << "2 - Task II" << endl;
+	// cout << "3 - Task III" << endl;
+	// cout << "4 - Task IV" << endl;
+	// cout << "5 - Task V" << endl;
+	// cout << "6 - Task VI" << endl;
+	// cout << "7 - Task VII" << endl;
+	// cout << "8 - Task VIII" << endl;
+	// cout << "9 - Task IX" << endl;
+	// cout << "10 - Task X" << endl;
+	switch (choice)
+	{
+	case 1:
+		taskI();
+	}
+}
+
+void taskI() 
+{
+
+	int userInp, max = 0;
+	div_t result;
+
+	cout << "Enter num: " << endl;
+	cin >> userInp;
+
+	while (userInp != max) {
+		result = div(userInp, 2);
+
+		userInp = result.quot;
+		cout << "chasnoe: " << result.quot << endl;
+	}
+	cout << "lol: " << userInp << endl;
+	cout << "ostatok: " << result.rem << endl;
 }
