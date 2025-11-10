3cafe401fea3fe764659695f410f71cf92c8af6f
ALT <119232131+Ik4rCat@users.noreply.github.com>
2025-11-10 08:47:44 +0300

    upd(CTA)<alt>

--- Cpp/consoleApps/CTApp/CTApp.cpp
+++ Cpp/consoleApps/CTApp/CTApp.cpp
@@ -3,56 +3,71 @@
 #include <locale>
 using namespace std;
 
-int main()
+/// HI, i need to make my taskes (。_。) 
+
+void taskI()
 {
-	/// HI, i need to make my taskes (。_。) 
-	Menu();
+
+	int userInp, result, max = 0;
+
+	cout << "Enter num: " << endl;
+	cin >> userInp;
+
+	while (userInp > 0) {
+		result = userInp % 10;
+		if (userInp > max) max = userInp;
+		userInp = userInp / 10;
+	}
+	cout << "result: " << max << endl;
 }
 
-void Menu() 
+void taskII()
+{
+	//dont do yet
+}
+
+void taskIII()
+{
+	//dont do yet
+}
+
+void Menu()
 {
 
 	int choice;
 
 	while (true) {
+		
+		cout << "\nSelect task: \n 1 - Task I \n 1 - Task II \n 1 - Task III" << endl;
+		cout << "" << endl;
 
-	}
-	cout << "Select task: \n 1 - Task I" << endl;
-	cout << "" << endl;
+		cin >> choice;
 
-	cin >> choice;
-
-	// cout << "2 - Task II" << endl;
-	// cout << "3 - Task III" << endl;
-	// cout << "4 - Task IV" << endl;
-	// cout << "5 - Task V" << endl;
-	// cout << "6 - Task VI" << endl;
-	// cout << "7 - Task VII" << endl;
-	// cout << "8 - Task VIII" << endl;
-	// cout << "9 - Task IX" << endl;
-	// cout << "10 - Task X" << endl;
-	switch (choice)
-	{
-	case 1:
-		taskI();
+		// cout << "2 - Task II" << endl;
+		// cout << "3 - Task III" << endl;
+		// cout << "4 - Task IV" << endl;
+		// cout << "5 - Task V" << endl;
+		// cout << "6 - Task VI" << endl;
+		// cout << "7 - Task VII" << endl;
+		// cout << "8 - Task VIII" << endl;
+		// cout << "9 - Task IX" << endl;
+		// cout << "10 - Task X" << endl;
+		switch (choice)
+		{
+		case 1:
+			taskI();
+			//case 2:
+			//	taskII();
+			//case 3:
+			//	taskIII();
+		}
 	}
 }
 
-void taskI() 
+//---------------------------------------
+
+int main()
 {
-
-	int userInp, max = 0;
-	div_t result;
-
-	cout << "Enter num: " << endl;
-	cin >> userInp;
-
-	while (userInp != max) {
-		result = div(userInp, 2);
-
-		userInp = result.quot;
-		cout << "chasnoe: " << result.quot << endl;
-	}
-	cout << "lol: " << userInp << endl;
-	cout << "ostatok: " << result.rem << endl;
+	setlocale(LC_ALL, "ru");
+	Menu();
 }
