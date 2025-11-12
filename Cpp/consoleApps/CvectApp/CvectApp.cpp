09e328342d05f69cf604e58bbb8b47dbefc1fd1d
ALT <119232131+Ik4rCat@users.noreply.github.com>
2025-11-12 12:11:46 +0300

    feat(CvectApp cpp)<alt>

--- Cpp/consoleApps/CvectApp/CvectApp.cpp
+++ Cpp/consoleApps/CvectApp/CvectApp.cpp
@@ -2,7 +2,19 @@
 #include <vector>
 using namespace std;
 
+/// <summary>
+/// ENG: HI! Today i will work with vectors ^o^
+/// RU: Привет! Сегодня я буду работать с векторами ^o^
+/// </summary>
+
 int main()
 {
-   
+	vector<int> v1;
+	vector<int> v2(6);
+	vector<int> v3(5,15);
+	vector<int> v4{ 1, 2, 3, 4, 5 };
+	vector<int> v5; (v1);
+
+
+
 }
