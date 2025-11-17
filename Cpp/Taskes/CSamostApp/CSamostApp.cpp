bb5ee447266a5d25bb342d1a860d61723571e444
Ik4rCat <119232131+Ik4rCat@users.noreply.github.com>
2025-11-17 20:47:39 +0300

    upl&feat(CSamostApp.cpp)<alt>

---
+++ Cpp/Taskes/CSamostApp/CSamostApp.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int userInp = 0;
+
+	cout << "Choice task: " << endl;
+	cout << " 1 - task I" << endl;
+	cout << " 2 - task II" << endl;
+	cout << " 3 - task III" << endl;
+	cout << " 4 - task IV" << endl;
+	cout << " 5 - task V" << endl;
+	cout << " 6 - task VI" << endl;
+	cout << " 7 - task VII" << endl;
+	cout << " 8 - task VIII" << endl;
+	cout << " 9 - task IX" << endl;
+	cout << " 10- task X" << endl;
+	cout << " 11- task XI" << endl;
+	cout << " 12- task XII" << endl;
+	cout << " 13- task XIII" << endl;
+	cout << " 14- task XIV" << endl;
+	cout << " 15- task XV" << endl;
+	cout << " 16- task XVI" << endl;
+	cout << " 17- task XVII" << endl;
+	cout << " 18- task XVIII" << endl;
+	cout << " 19- task XIX" << endl;
+	cout << " 20- task XX" << endl;
+	cout << " 21- task XXI" << endl;
+	cout << " 22- task XXII" << endl;
+	cout << " 23- task XXIII" << endl;
+	cout << " 24- task XXIV" << endl;
+	cout << " 25- task XXV" << endl;
+	cout << " 26- task XVI" << endl;
+	cout << " 0- exit" << endl;
+	cin >> userInp;
+
+	switch (userInp)
+	{
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	case 7:
+		break;
+	case 8:
+		break;
+	case 9:
+		break;
+	case 10:
+		break;
+	case 11:
+		break;
+	case 12:
+		break;
+	case 13:
+		break;
+	case 14:
+		break;
+	case 15:
+		break;
+	case 16:
+		break;
+	case 17:
+		break;
+	case 18:
+		break;
+	case 19:
+		break;
+	case 20:
+		break;
+	case 21:
+		break;
+	case 22:
+		break;
+	case 23:
+		break;
+	case 24:
+		break;
+	case 25:
+		break;
+	case 26:
+		break;
+	case 0:
+		break;
+	}
+}
