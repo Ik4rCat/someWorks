d70adb776696e67a59db23a16f1e5e6e3842bf24
ALT <119232131+Ik4rCat@users.noreply.github.com>
2025-11-18 13:21:22 +0300

    upd(CT1811A)<alt>

--- Cpp/consoleApps/CT1811App/CT1811App.cpp
+++ Cpp/consoleApps/CT1811App/CT1811App.cpp
@@ -3,48 +3,118 @@
 #include <vector>
 #include <random>
 #include <ctime>
+#include <cmath>
 using namespace std;
 
 void TaskI() 
 {
-	int i;
-
 	int nums[15]{};
-	
 	vector<int> v;
 
-	for (i = 0; i < size(nums); i++) {
+	for (int i = 0; i < size(nums); i++) {
 		nums[i] = rand();
 	}
 
-	while (next_permutation(v.begin(), v.end()))
-	{
-		copy(v.begin(), v.end(), ostream_iterator<int>(cout, ""));
-		cout << endl;
-	}
+	copy(begin(nums), end(nums), back_inserter(v));
 
+	sort(v.begin(), v.end());
+
+	do {
+		copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+		cout << endl;
+	} while (next_permutation(v.begin(), v.end()));
 
 }
 
 void TaskII() 
 {
+	int i;
+
 	int num[10]{ 1,2,3,4,5,6,7,8,9,0 };
+	int setSizes[] = { 10, 26, 26, 7 };
 
 	char lletter[26]{ 'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m' };
 	char bletter[26]{ 'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M' };
 	char symb[7]{ '!','@','#','$','%','&','*' };
+	char pass[9]{};
 
+	char* charSets[] = {
+	reinterpret_cast<char*>(num),
+	lletter,
+	bletter,
+	symb
+	};
 
+	for (int i = 0; i < size(pass) - 1; i++) { 
+		int setIndex = rand() % 4;
 
+		int charIndex = rand() % setSizes[setIndex];
+
+		if (setIndex == 0) {
+			pass[i] = '0' + num[charIndex];
+		}
+		else {
+			pass[i] = charSets[setIndex][charIndex];
+		}
+	}
+
+	pass[8] = '\0';
+
+	cout << "pass: " << pass << endl;
 
 }
 
-void TaskIII() {
+void TaskIII() 
+{
+
+
+	int number = 0;
+	int temp = number;
+	int divisor = 2;
+	vector<int> factors;
+
+	cout << "Enter natur num: ";
+	cin >> number;
+
+	if (number <= 1) {
+		cout << "need num bigger 1 !" << endl;
+		return;
+	}
+
+	cout << "\nprocess on mini nums:" << endl;
+	cout << number;
+
+	while (temp > 1) {
+		if (temp % divisor == 0) {
+			factors.push_back(divisor);
+			temp /= divisor;
+			cout << " | " << divisor << endl;
+			if (temp > 1) {
+				cout << temp;
+			}
+		}
+		else {
+			divisor++;
+			if (divisor > 2 && divisor % 2 == 0) divisor++;
+		}
+	}
+
+	cout << " | 1" << endl << endl;
+	cout << number << " = ";
+
+	for (size_t i = 0; i < factors.size(); ++i) {
+		cout << factors[i];
+		if (i < factors.size() - 1) {
+			cout << " * ";
+		}
+	}
+	cout << endl;
 
 }
 
-void TaskIV() {
-
+void TaskIV() 
+{
+	cout << "in working (dont realized yet :( )" << endl;
 }
 
 int main()
