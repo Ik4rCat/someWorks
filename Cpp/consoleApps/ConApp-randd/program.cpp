befd5510ad3729f3ef4b45aeda882203ca0f209b
Ik4rCat <altworsekitten@gmail.com>
2026-01-22 14:46:25 +0300

    error? idk

--- Cpp/consoleApps/ConApp-randd/program.cpp
+++ Cpp/consoleApps/ConApp-randd/program.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
 #include <random>
-
 using namespace std;
+
+int RGenerator(int min, int max, int val){
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dist(min, max);
+
+    val = dist(gen);
+}
+
+int GenD(int dayN, int mounth){
+    int minDataVal= 1 , maxDataVal= 31;
+
+    //if(mounth == )
+
+    RGenerator(minDataVal, maxDataVal, dayN);
+
+}
+
+int GenM(int mounthN){
+    int minDataVal= 1 , maxDataVal= 12;
+
+    RGenerator(minDataVal, maxDataVal, mounthN);
+}
+
+int GenY(int yearN){
+    int minDataVal= 1 , maxDataVal= 3000;
+
+    RGenerator(minDataVal, maxDataVal, yearN);
+}
 
 int main()
 {
-    return 0;
+    int day, mounth, year;
+
+    cout<<"Starting generation"<<endl;
+
+    GenY(year);
+    GenM(mounth);
+    GenD(day, mounth);
+
+    cout<<"your data: "<< day << "/" << mounth << "/" << year <<endl;
+
 }
