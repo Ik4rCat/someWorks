d61d19f5392aadf2f1756f25d3912d9df9571ee8
Ik4rCat <altworsekitten@gmail.com>
2026-03-27 14:46:12 +0300

    examSavePoint14:50

--- Cpp/-OOPcppExam270326/Program.cpp
+++ Cpp/-OOPcppExam270326/Program.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "Point.h"
 #include "Vector.h"
@@ -7,17 +8,26 @@ using namespace std;
 
 int main()
 {
-    Vector<int> vec1{ 1, 2, 3, 4, 5 };
-    vec1.print();
+    Vector<int> vecI{ 1, 2, 3, 4, 5 };
+    vecI.print();
 
-    Vector<double> vec2(3);
-    vec2[0] = 1.5;
-    vec2[1] = 2.5;
-    vec2[2] = 3.5;
-    vec2.print();
+    Vector<double> vecII(3);
+    vecII[0] = 1.5;
+    vecII[1] = 2.5;
+    vecII[2] = 3.5;
+    vecII.print();
 
-    calculateAndPrint(vec1, 1, 2.0, 1);
-    calculateAndPrint(vec1, 10, 0.0, 1);
+    Vector<string> vecIII{ " ", "funny", "Vector", "task", "Point class With <3"};
+    vecIII.print();
+    cout << vecIII[1] << vecIII[0] << vecIII[2] << vecIII[0] << vecIII[3] << endl;
+    cout << vecIII[4] << endl;
+
+    Vector<float> vecIV { 3.14 };
+    cout << vecIV[0] << endl;
+    vecIV.print();
+
+    calculateAndPrint(vecI, 1, 2.0, 1);
+    calculateAndPrint(vecI, 10, 0.0, 1);
 
     Point p(10.0, 20.0);
     p.show();
