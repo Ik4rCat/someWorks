28308ac6b92d237e2a01326373db489f47ab4986
Ik4rCat <altworsekitten@gmail.com>
2026-03-27 16:10:13 +0300

    examSavePoint

--- Cpp/-OOPcppExam270326/Program.cpp
+++ Cpp/-OOPcppExam270326/Program.cpp
@@ -8,27 +8,34 @@ using namespace std;
 
 int main()
 {
-    cout << "int vector: " << endl;
+
+    cout << "empty vector: " << endl;
+    Vector<int> vecnull;
+    vecnull.print();
+    cout << "empty: " << (vecnull.empty() ? "true" : "false") << endl;
+
+
+    cout << "\nint vector: " << endl;
     Vector<int> vecI{ 1, 2, 3, 4, 5 };
     vecI.print();
 
-    cout << "empty: " << (vecI.empty() ? "true" : "false") << endl;
+    cout << "\nempty: " << (vecI.empty() ? "true" : "false") << endl;
     cout << "size: " << vecI.size() << endl;
 
     vecI.push(6);
-    cout << "after push(6): " << endl;
+    cout << "\nafter push(6): " << endl;
     vecI.print();
 
     vecI.pop();
-    cout << "after pop(): " << endl;
+    cout << "\nafter pop(): " << endl;
     vecI.print();
 
     vecI.insert(2, 99);
-    cout << "after insert(2, 99): " << endl;
+    cout << "\nafter insert(2, 99): " << endl;
     vecI.print();
 
     vecI.resize(3);
-    cout << "after resize(3): " << endl;
+    cout << "\nafter resize(3): " << endl;
     vecI.print();
 
 
@@ -39,17 +46,21 @@ int main()
     vecII[2] = 3.5;
     vecII.print();
 
+
     cout << "\nstring vector: " << endl;
     Vector<string> vecIII{ " ", "funny", "Vector", "task", "Point class With <3"};
     vecIII.print();
+
 
     cout << "\nfloat vector: " << endl;
     Vector<float> vecIV { 3.143, 3.154, 3.165 };
     vecIV.print();
 
+
     cout << "\nPoint's vector: " << endl;
     Vector<Point> vecV{ Point(0, 0), Point(1, 1), Point(2, 2), Point(2, 4), Point(4, 2)};
     vecV.print();
+
 
     cout << "\nCalculate & print (int+double)/long:" << endl;
     calculateAndPrint(vecI, 1, 2.0, 2L);
