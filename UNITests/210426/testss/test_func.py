451695450b9cf842b907b2413bbf703bce37bd6b
Ik4rCat <altworsekitten@gmail.com>
2026-04-21 13:36:37 +0300

    udp

--- UNITests/210426/testss/test_func.py
+++ UNITests/210426/testss/test_func.py
@@ -1,10 +1,7 @@
-from pathlib import Path
-import sys
-
-sys.path.append(str(Path(__file__).resolve().parents[1]))
-
 from utils import division
 
+
+@pytest
 
 def test_division_success():
     assert division(10, 2) == 5
