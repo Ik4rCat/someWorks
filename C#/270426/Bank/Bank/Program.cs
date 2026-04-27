d5cc1391c02dad58207ee09078c1b7323ff62458
Ik4rCat <altworsekitten@gmail.com>
2026-04-27 23:23:53 +0300

    upd

---
+++ C#/270426/Bank/Bank/Program.cs
@@ -0,0 +1,47 @@
+﻿
+class Bank
+{
+    
+}
+
+class BankAccount
+{
+    private int balance;
+    private int id;
+    private int counterOfGetOf;
+    private int dateOfGetOf;
+    
+    
+
+    public int Balance
+    {
+        get => balance;
+        set => balance = value;
+    }
+    
+    public int Id
+    {
+        get => id;
+        set => id = value;
+    }
+
+    public int CounterOfGetOf
+    {
+        get => counterOfGetOf;
+        set => counterOfGetOf = value;
+    }
+
+    public int DateOfGetOf
+    {
+        get => dateOfGetOf;
+        set => dateOfGetOf = value;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        
+    }
+}
