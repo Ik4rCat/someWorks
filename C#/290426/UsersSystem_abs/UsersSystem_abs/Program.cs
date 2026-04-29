9ca0b386be54a9634bfec9cb464d402b8644f787
Ik4rCat <altworsekitten@gmail.com>
2026-04-29 17:07:37 +0300

    upd

---
+++ C#/290426/UsersSystem_abs/UsersSystem_abs/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+class User
+{
+    private string _name;
+    private string _email;
+
+    public User(string name, string email)
+    {
+        _name = name;
+        _email = email;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetEmail()
+    {
+        return _email;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        
+    }
+}
