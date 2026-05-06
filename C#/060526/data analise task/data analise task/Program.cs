9336c85c504d8bc26e7fd8c3da6d10be29dd0819
Ik4rCat <altworsekitten@gmail.com>
2026-05-06 18:02:17 +0300

    upd

---
+++ C#/060526/data analise task/data analise task/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main()
+    {
+        ArrayList data = new ArrayList()
+        {
+            23, "Error 404", true, 17, false, "Error 500", true, 31, true, "Error 403"
+        };
+
+        List<int> temperatures = new List<int>();
+        List<string> errors = new List<string>();
+        int activeCount = 0;
+
+        foreach (var item in data)
+        {
+            if (item is int temp)
+                temperatures.Add(temp);
+            else if (item is string err)
+                errors.Add(err);
+            else if (item is bool status && status == true)
+                activeCount++;
+        }
+        
+        int sum = 0;
+        for (int i = 0; i < temperatures.Count; i++)
+            sum += temperatures[i];
+        double avg = (double)sum / temperatures.Count;
+        
+        string allErrors = string.Join(", ", errors);
+
+        Console.WriteLine($"Среднее температур: {avg:F1}");
+        Console.WriteLine($"Ошибки: {allErrors}");
+        Console.WriteLine($"Активных состояний: {activeCount}");
+    }
+}
