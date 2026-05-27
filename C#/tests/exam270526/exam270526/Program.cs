6fa2fa1ffc1bab0dff0cf94edbd123042d3de6fd
Ik4rCat <altworsekitten@gmail.com>
2026-05-27 14:36:32 +0300

    upd

---
+++ C#/tests/exam270526/exam270526/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Program
+{
+    private int userNum, nNum;
+    private bool isReal;
+    
+    void CheckIsRealN(int uNum)
+    {
+        int resl = 2;
+        
+        for (int i = 0; i < uNum; i++)
+        {
+            resl = resl * 2;
+        }
+        
+        Console.WriteLine($"num is {isReal}");
+    }
+    
+    public static void Main()
+    {
+        Console.WriteLine("Write num: ");
+        CheckIsRealN(Console.ReadLine());
+        
+    }
+}
