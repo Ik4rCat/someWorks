71c7c301aaa165146fcf33952e9497fc914b5edb
Ik4rCat <altworsekitten@gmail.com>
2026-06-01 13:20:38 +0300

    upload

--- C#/DevelopmentLessonTaskII010626/DevelopmentLessonTaskII010626/Program.cs
+++ C#/DevelopmentLessonTaskII010626/DevelopmentLessonTaskII010626/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Data;
-using Npgsql;
 
 namespace DevelopmentLessonTaskII010626
 {
@@ -8,50 +7,40 @@ namespace DevelopmentLessonTaskII010626
     {
         static void Main(string[] args)
         {
-            string connectionString = "";
-            string sql = "SELECT * FROM Users";
-            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            DataSet usersSet = new DataSet("UsersSet");
+            DataTable users = new DataTable("Users");
+            usersSet.Tables.Add(users);
+
+            DataColumn idColumn = new DataColumn("Id", Type.GetType("System.Int32"));
+            idColumn.Unique = true;
+            idColumn.AllowDBNull = false;
+            idColumn.AutoIncrement = true;
+            idColumn.AutoIncrementSeed = 1;
+            idColumn.AutoIncrementStep = 1;
+
+            DataColumn nameColumn = new DataColumn("Name", Type.GetType("System.String"));
+            DataColumn ageColumn = new DataColumn("Age", Type.GetType("System.Int32"));
+            ageColumn.DefaultValue = 1;
+
+            users.Columns.Add(idColumn);
+            users.Columns.Add(nameColumn);
+            users.Columns.Add(ageColumn);
+            users.PrimaryKey = new DataColumn[] { users.Columns["Id"] };
+
+            DataRow row = users.NewRow();
+            row.ItemArray = new object[] { null, "Tom", 36 };
+            users.Rows.Add(row);
+            users.Rows.Add(new object[] { null, "Bob", 29 });
+
+            Console.Write("Id\t Имя\t Возраст\t");
+            Console.WriteLine();
+            foreach (DataRow r in users.Rows)
             {
-                // Создаем объект DataAdapter
-                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(sql, connection);
-                // Создаем объект Dataset
-                DataSet ds = new DataSet();
-                // Заполняем Dataset
-                adapter.Fill(ds);
-                DataTable dt = ds.Tables[0];
-                // добавим новую строку
-                DataRow newRow = dt.NewRow();
-                newRow["Name"] = "Rick";
-                newRow["Age"] = 24;
-                dt.Rows.Add(newRow);
-                // Изменим значение в столбце Age для первой строки
-                dt.Rows[0]["Age"] = 17;
-                // создаем объект SqlCommandBuilder
-                NpgsqlCommandBuilder commandBuilder = new NpgsqlCommandBuilder(adapter);
-                adapter.Update(ds);
-                // альтернативный способ - обновление только одной таблицы
-                //adapter.Update(dt);
-                // заново получаем данные из бд
-                // очищаем полностью DataSet
-                ds.Clear();
-                // перезагружаем данные
-                adapter.Fill(ds);
-                // Отображаем данные
-                // перебор всех таблиц
-                foreach (DataColumn column in dt.Columns)
-                    Console.Write($"{column.ColumnName}\t");
+                foreach (var cell in r.ItemArray)
+                    Console.Write("{0}\t", cell);
                 Console.WriteLine();
-                // перебор всех строк таблицы
-                foreach (DataRow row in dt.Rows)
-                {
-                    // получаем все ячейки строки
-                    var cells = row.ItemArray;
-                    foreach (object cell in cells)
-                        Console.Write($"{cell}\t");
-                    Console.WriteLine();
-                }
-                Console.Read();
             }
+            Console.Read();
         }
     }
 }
